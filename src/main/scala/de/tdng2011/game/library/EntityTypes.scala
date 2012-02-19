b6fc118f8afaa29c1cdcaa6451349c3de757a766
Felix Becker <git@felixbecker.name>
2012-02-19 19:22:52 +0100

    new message types

--- src/main/scala/de/tdng2011/game/library/EntityTypes.scala
+++ src/main/scala/de/tdng2011/game/library/EntityTypes.scala
@@ -2,15 +2,21 @@ package de.tdng2011.game.library
 
 object EntityTypes extends Enumeration {
   val
-    Player,
-    Shot,
-    PowerUp,
-    World,
-    Handshake,
-    Action,
-    ScoreBoard,
-    PlayerJoined,
-    PlayerLeft,
-    PlayerName
+    Player,  // 0
+    Shot,    // 1
+    PowerUp, // 2
+    World,   // 3
+    Handshake, // 4
+    Action,    // 5
+    ScoreBoard, // 6
+    PlayerJoined, // 7
+    PlayerLeft,   // 8
+    PlayerName,    // 9
+    // NG Messages
+    PlayerKilledEvent,  // 10
+    PlayerCollisionEvent, // 11
+    ShotCollisionEvent, // 12
+    PlayerSpawnedEvent, // 13
+    ShotSpawnedEvent // 14
   = Value
 }
